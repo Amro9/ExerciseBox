737c4f1f0ac7bd5a5222317422b7e59559278f17
thomas.klettner <tomas.klettner@gmail.com>
2024-05-29 16:33:41 +0200

    new components

--- exercisesBox.UI/exerciseBox.Web/src/app/app.routing.module.ts
+++ exercisesBox.UI/exerciseBox.Web/src/app/app.routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { AdminViewComponent } from "./Components/admin-view/admin-view.component";
 import { AppComponent } from "./app.component";
 import { QuestionCreationFormComponent } from "./question-form/question-form.component";
+import { LoginComponent } from "./Components/login/login.component";
 
 
 const routes: Routes = [
   { path: "adminView", component: AdminViewComponent },
   { path: 'questionForm', component: QuestionCreationFormComponent},
   { path: "home", component: AppComponent },
+  { path: "login", component: LoginComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   ];
 
@@ -21,6 +23,7 @@ const routes: Routes = [
   export const routingComponents = 
   [ 
     AdminViewComponent,
-    QuestionCreationFormComponent
+    QuestionCreationFormComponent,
+    LoginComponent
   ]
   
