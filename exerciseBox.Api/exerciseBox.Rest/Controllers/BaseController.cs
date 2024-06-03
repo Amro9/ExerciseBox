8a5cd5be69f9bca2900059988c12c30cb2be70e0
amro9 <amrorehawi@gmail.com>
2024-06-03 13:55:41 +0200

    basecontroller

--- exerciseBox.Api/exerciseBox.Rest/Controllers/BaseController.cs
+++ exerciseBox.Api/exerciseBox.Rest/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using MediatR;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace exerciseBox.Rest.Controllers;
@@ -7,7 +8,11 @@ namespace exerciseBox.Rest.Controllers;
 [Produces("application/json")]
 [Route("api/[controller]")]
 [EnableCors("AllowAllOrigins")]
-public abstract class BaseController
+public abstract class BaseController : Controller
 {
-
+    protected readonly IMediator _mediator;
+    public BaseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
 }
